eef72525323f57e92d9f12ec24932d85f3c12724
Brian Meek <brian@here.video>
2022-01-16 08:57:05 -0800

    Update to latest fastify and fastify-static

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  ignorePatterns: ["build/**/*.js", "samples/**/*.js", "third_party/**/*.js"],
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {},
+};
--- ts/src/index.ts
+++ ts/src/index.ts
@@ -12,16 +12,21 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import * as cookie from 'cookie';
-import * as fastify from 'fastify';
-import fp from 'fastify-plugin';
-import fastifyStatic from 'fastify-static';
-import * as autoPush from 'h2-auto-push';
-import * as http from 'http';
-import * as http2 from 'http2';
-import * as https from 'https';
+import * as cookie from "cookie";
+import * as fastify from "fastify";
+import {
+  FastifyLoggerInstance,
+  RawReplyDefaultExpression,
+  RawRequestDefaultExpression,
+  RawServerBase,
+} from "fastify";
+import fastifyStatic from "fastify-static";
+import * as autoPush from "h2-auto-push";
+import * as http from "http";
+import * as http2 from "http2";
+import * as https from "https";
 
-export { AssetCacheConfig } from 'h2-auto-push';
+export { AssetCacheConfig } from "h2-auto-push";
 
 export type HttpServer =
   | http.Server
@@ -30,14 +35,8 @@ export type HttpServer =
   | http2.Http2SecureServer;
 export type RawRequest = http.IncomingMessage | http2.Http2ServerRequest;
 export type RawResponse = http.ServerResponse | http2.Http2ServerResponse;
-type Request = fastify.FastifyRequest<RawRequest>;
-type Response = fastify.FastifyReply<RawResponse>;
 
-export interface AutoPushOptions<
-  Server extends HttpServer,
-  Request extends RawRequest,
-  Response extends RawResponse
-> extends fastify.RegisterOptions<Server, Request, Response> {
+export interface AutoPushOptions extends fastify.RegisterOptions {
   root: string;
   prefix?: string;
   cacheConfig?: autoPush.AssetCacheConfig;
@@ -51,31 +50,32 @@ function isHttp2Response(res: RawResponse): res is http2.Http2ServerResponse {
   return !!(res as http2.Http2ServerResponse).stream;
 }
 
-const CACHE_COOKIE_KEY = '__ap_cache__';
+const CACHE_COOKIE_KEY = "__ap_cache__";
 
 export async function staticServeFn<
-  Server extends HttpServer,
-  Request extends RawRequest,
-  Response extends RawResponse
+  Server extends RawServerBase,
+  Request extends RawRequestDefaultExpression<Server>,
+  Response extends RawReplyDefaultExpression<Server>,
+  Logger extends FastifyLoggerInstance
 >(
-  app: fastify.FastifyInstance<Server, Request, Response>,
-  opts: AutoPushOptions<Server, Request, Response>
+  app: fastify.FastifyInstance<Server, Request, Response, Logger>,
+  opts: AutoPushOptions
 ): Promise<void> {
   const root = opts.root;
-  const prefix = opts.prefix || '/';
+  const prefix = opts.prefix || "/";
   const ap = new autoPush.AutoPush(root, opts.cacheConfig);
 
   app.register(fastifyStatic, opts);
 
-  app.addHook('onRequest', async (req, res) => {
+  app.addHook("onRequest", async (req, res) => {
     // Used for compatibility with fastify 1.x and 2.0
     const rawRequest = req.raw || req;
-    const rawResponse = res.res || res;
+    const rawResponse = res.raw || res;
     if (isHttp2Request(rawRequest)) {
       const reqPath = rawRequest.url;
       const reqStream = rawRequest.stream;
       const cookies = cookie.parse(
-        (rawRequest.headers['cookie'] as string) || ''
+        (rawRequest.headers["cookie"] as string) || ""
       );
       const cacheKey = cookies[CACHE_COOKIE_KEY];
       const { newCacheCookie, pushFn } = await ap.preprocessRequest(
@@ -85,23 +85,23 @@ export async function staticServeFn<
       );
       // TODO(jinwoo): Consider making this persistent across sessions.
       rawResponse.setHeader(
-        'set-cookie',
-        cookie.serialize(CACHE_COOKIE_KEY, newCacheCookie, { path: '/' })
+        "set-cookie",
+        cookie.serialize(CACHE_COOKIE_KEY, newCacheCookie, { path: "/" })
       );
 
-      reqStream.on('pushError', err => {
-        req.log!.error('Error while pushing', err);
+      reqStream.on("pushError", (err) => {
+        req.log?.error("Error while pushing", err);
       });
       pushFn().then(noop, noop);
     }
   });
 
-  app.addHook('onSend', async (request, reply, payload) => {
-    const res = reply.res;
-    if (isHttp2Response(res)) {
-      const statusCode = res.statusCode;
-      const resStream = res.stream;
-      const reqPath = request.req.url;
+  app.addHook("onSend", async (request, reply) => {
+    const raw = reply.raw;
+    if (isHttp2Response(raw)) {
+      const statusCode = raw.statusCode;
+      const resStream = raw.stream;
+      const reqPath = request.raw.url;
       if (statusCode === 404 && reqPath) {
         ap.recordRequestPath(resStream.session, reqPath, false);
       } else if (
@@ -117,14 +117,17 @@ export async function staticServeFn<
   });
 }
 
+// eslint-disable-next-line @typescript-eslint/no-empty-function
 function noop() {}
 
 // This type is probably not useful. Users probably want to call fp to
 // instantiate the plugin specifically for their specific Http server, request
 // and response types rather than using the union types like we do here.
+/*
 export const staticServe = fp<
   HttpServer,
   RawRequest,
   RawResponse,
   AutoPushOptions<HttpServer, RawRequest, RawResponse>
 >(staticServeFn);
+*/
